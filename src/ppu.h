d2ed6faca1805236246a3b959965da3d8dd977f5
= <joseguilhermebh@hotmail.com>
2021-03-22 21:52:22 -0300

    Added OAM memory to PPU

--- src/ppu.h
+++ src/ppu.h
@@ -3,10 +3,20 @@
 
 #include <stdint.h>
 
+#define OAM_ENTRY_NUM 64
+
 typedef struct _Memory Memory;
+
+typedef struct _OAMEntry {
+    uint8_t y;
+    uint8_t tileNumber;
+    uint8_t spriteAttr;
+    uint8_t x;
+} OAMEntry;
 
 typedef struct _Ppu {
     Memory *mem;
+    OAMEntry oamMemory[OAM_ENTRY_NUM];
 
     uint8_t oddFrame;
     uint16_t scanline;
